762d71984c2469b9c508114c7c6bde5a125c1b89
EitanVeryKatz <eitanka@mta.ac.il>
2025-05-21 13:38:06 +0300

    added vehicle, wheel, and fueal car

---
+++ Ex03.GarageLogic/FuelCar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    public class FuelCar:Vehicle
+    {
+        private const int k_NumOfWheels = 4;
+        private const float k_MaximunWheelAirPressure = 32;
+        FuelCar(string i_ModelName,string i_LicenseID ) :base(i_ModelName,i_LicenseID)
+        {
+            base.m_Wheels = new Wheel[k_NumOfWheels];
+            for(int i = 0; i < k_NumOfWheels; i++)
+            {
+                m_Wheels[i] = new Wheel(k_MaximunWheelAirPressure);
+            }
+        }
+    }
+}
---
+++ Ex03.GarageLogic/Vehicle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    public abstract class Vehicle
+    {
+        public readonly string r_ModelName;
+        public readonly string r_LicenseID;
+        public float EnergySourcePrecentage {  get; set; }
+
+        protected Wheel[] m_Wheels;
+
+        protected class Wheel
+        {
+            public string m_ManufacturerName;
+            public readonly float r_MaximumAllowedAirPressure;
+           
+            public float CurrentAirPressure {  get; set; }
+
+            public Wheel(float i_MaximumAllowedAirPressure)
+            {
+                r_MaximumAllowedAirPressure = i_MaximumAllowedAirPressure;
+            }
+
+            public void AddAir(float i_airToAdd)
+            {
+                if(CurrentAirPressure + i_airToAdd <= r_MaximumAllowedAirPressure)
+                {
+                    CurrentAirPressure += i_airToAdd;
+                }
+
+            }
+        }
+
+        public Vehicle(string i_ModelName, string i_LicenseID) 
+        {
+            r_ModelName = i_ModelName;
+            r_LicenseID = i_LicenseID;
+        }
+
+        
+    }
+}
