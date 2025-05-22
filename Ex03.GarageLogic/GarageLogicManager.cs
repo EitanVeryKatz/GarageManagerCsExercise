b0e7110f98a3aa33ba13bd88436b782cae399ff8
EitanVeryKatz <eitanka@mta.ac.il>
2025-05-22 15:24:56 +0300

    added garage maneger logic and began ui

---
+++ Ex03.GarageLogic/GarageLogicManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.GarageLogic
+{
+    public class GarageLogicManager
+    {
+        public readonly Dictionary<Vehicle, VehicleDataAndStatus> m_vehicles = new Dictionary<Vehicle, VehicleDataAndStatus>();
+
+        public class VehicleDataAndStatus
+        {
+            private e_StatusOfVehicleInGarage m_status = e_StatusOfVehicleInGarage.WorkInProgress;
+            public string OwnerName { get; private set; }
+            public string OwnerPhoneNumber { get; private set; }
+
+            public e_StatusOfVehicleInGarage Status
+            {
+                get
+                {
+                    return m_status;
+                }
+                set 
+                {
+                    m_status = value;
+                }
+
+            }
+            
+        }
+
+        public enum e_StatusOfVehicleInGarage
+        {
+            WorkInProgress,
+            WorkFinished,
+            Paid
+        }
+    }
+}
